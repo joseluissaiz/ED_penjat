6ff0eb3f40288e6aa5436b16b0db3de11622f7a7
Jose Luis Saiz <jsaiz@cifpfbmoll.eu>
2021-02-10 16:30:38 +0100

    Proyecto creado
    He copiado los archivos del proyeto para poder trabajar con el nuevo archivo añadido "Tauler.java" (A partir de aquí haremos distintas ramas para cada test que realizemos).

---
+++ src/com/jaume/penjat/Tauler.java
@@ -0,0 +1,27 @@
+package com.jaume.penjat;
+
+public class Tauler {
+    public void inicialitzarPartida(String paraula, int i) {
+    }
+
+    public char[] getParaulaSecreta() {
+    }
+
+    public int getIntents() {
+    }
+
+    public String verificar(String ll) {
+    }
+
+    public Object[] getPalabraEndevinada() {
+    }
+
+    public String imprimir() {
+    }
+
+    public String imprimirVides() {
+    }
+
+    public boolean hasGuanyat() {
+    }
+}
